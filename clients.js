ca2ecb13545755f1379c50920dbcfeffa671b7bb
santi <santiago.bolanop@gmail.com>
2024-03-06 07:34:12 -0700

    added add clients with correct format

--- clients.js
+++ clients.js
@@ -1,1 +1,45 @@
+let list1 = []
+let list2 = []
+let list3 = []
+let list4 = []
+
+let n= 1;
+let x =0;
+let ids = 0;
+const now = moment(); // Get the current date and time
+const formattedDate = now.format('ll');
+
+function addRow(){
+    
+    let addRown = document.getElementById("show")
+    let newRow = addRown.insertRow(n);
+    list1[x] = ids;
+    list2[x] = document.getElementById('cliente').value
+    list3[x] = document.getElementById('amount').value
+    list4[x] = formattedDate
+    
+    let cel1 = newRow.insertCell(0)
+    let cel2 = newRow.insertCell(1);
+	let cel3 = newRow.insertCell(2);
+    let cel4 = newRow.insertCell(3)
+
+    cel1.innerHTML = list1[x];
+	cel2.innerHTML = list2[x];
+    cel3.innerHTML = list3[x];
+    cel4.innerHTML = list4[x];
+
+    n++;
+    x++;
+    ids++;
+    console.log("enter here2")
+}
+
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
 
--- login.js
+++ login.js
@@ -3,26 +3,25 @@ let elName = document.querySelector("#name");
 function login(){
 
 localStorage.setItem("userName",elName.value);
-console.log("sapo0");
+
 window.location.href="clients.html";
 
 } 
 
-console.log("sapo1");
 // const btnEl = document.querySelector("#test");
 
 // const headEl = document.querySelector("#welName")
 
 
 window.onload = function() {
-    console.log("sapo2");
+   
     const headEl = document.querySelector("#welName");
     const userName = localStorage.getItem("userName");
     if (userName) {
         headEl.innerHTML = ` Welcome ${userName} !`;
-        console.log("sapo2.1");
+        
     }
-    console.log("sapo3");
+   
 };
 
         
