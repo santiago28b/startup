3da56567464cf270df088ad51638dd40e4e9355f
santi <santiago.bolanop@gmail.com>
2024-04-10 16:10:37 -0600

    added local storage to employee

--- employee.js
+++ employee.js
@@ -22,16 +22,50 @@ function addRow(){
     let cel4 = newRow.insertCell(3)
 
     cel1.innerHTML = list1[x];
-	cel2.innerHTML = list2[x];
+	  cel2.innerHTML = list2[x];
     cel3.innerHTML = list3[x];
     cel4.innerHTML = list4[x];
 
     console.log(list4[x])
+    saveToLocalStorage();
     n++;
     x++;
     ids++;
     console.log("enter here2")
 }
+
+function saveToLocalStorage() {
+  localStorage.setItem('list1', JSON.stringify(list1));
+  localStorage.setItem('list2', JSON.stringify(list2));
+  localStorage.setItem('list3', JSON.stringify(list3));
+  localStorage.setItem('list4', JSON.stringify(list4));
+}
+
+function loadFromLocalStorage() {
+  list1 = JSON.parse(localStorage.getItem('list1')) || []; //it retrieves what is stored in local storage or gets an empty array
+  list2 = JSON.parse(localStorage.getItem('list2')) || [];
+  list3 = JSON.parse(localStorage.getItem('list3')) || [];
+  list4 = JSON.parse(localStorage.getItem('list4')) || [];
+  console.log("entered here");
+  let table = document.getElementById('show');
+  //table.innerHTML = ''; // Clear existing table rows
+  
+  for (let i = 0; i < list1.length; i++) {
+    let newRow = table.insertRow(i + 1); // Add 1 to account for table header row
+    
+    let cel1 = newRow.insertCell(0);
+    let cel2 = newRow.insertCell(1);
+    let cel3 = newRow.insertCell(2);
+    let cel4 = newRow.insertCell(3);
+    
+    cel1.innerHTML = list1[i];
+    cel2.innerHTML = list2[i];
+    cel3.innerHTML = list3[i];
+    cel4.innerHTML = list4[i];
+  }
+}
+
+window.onload = loadFromLocalStorage();
 
 
 function openAddForm() {
